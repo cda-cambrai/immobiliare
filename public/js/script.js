b75e5222334a86b8acbc7b3bba51f00670fc4880
Matthieu Mota <matthieumota@gmail.com>
2021-01-19 08:05:38 +0100

    Create real estate

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,16 @@ $('#ajax-properties').click(function () {
         console.log(properties);
     });
 });
+
+// Dès qu'on arrive sur l'ajout d'annonce, on affiche tout de suite la valeur du input range
+$('#real_estate_surface').after('<div id="result">'+$('#real_estate_surface').val()+' m²</div>');
+
+/**
+ * On écoute l'événement sur le range
+ */
+$('#real_estate_surface').on('input', function () {
+    // alert('Toto');
+    $('#result').remove(); // On supprime la div pour éviter les doublons
+    // Je récupère la valeur du input et l'ajouter directement en dessous de celui-ci
+    $(this).after('<div id="result">'+$(this).val()+' m²</div>');
+});
--- src/Form/RealEstateType.php
+++ src/Form/RealEstateType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\RealEstate;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +16,41 @@ class RealEstateType extends AbstractType
         $builder
             ->add('title')
             ->add('description')
-            ->add('surface')
-            ->add('price')
-            ->add('rooms')
-            ->add('type')
-            ->add('sold')
+            ->add('surface', RangeType::class, [
+                // On configure un <input type="range" min="10" max="400">
+                'attr' => [
+                    'min' => 10,
+                    'max' => 400,
+                    'class' => 'p-0', // Ajoute une classe sur le input
+                ],
+            ])
+            ->add('price', null, [
+                // On peut définir le label directement dans le PHP
+                'label' => 'Prix',
+            ])
+            // On peut changer le type du champ avec le second paramètre
+            ->add('rooms', ChoiceType::class, [
+                // Attention, <option value="VALUE">KEY</option>
+                'choices' => [
+                    'Studio' => 1,
+                    'T2' => 2,
+                    'T3' => 3,
+                    'T4' => 4,
+                    'T5' => 5,
+                ],
+                'label' => 'Nombre de pièces',
+            ])
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Maison' => 'maison',
+                    'Appartement' => 'appartement',
+                ],
+                // Pour avoir des radios au lieu du select
+                'expanded' => true,
+            ])
+            ->add('sold', null, [
+                'label' => 'Vendu ?',
+            ])
         ;
     }
 
